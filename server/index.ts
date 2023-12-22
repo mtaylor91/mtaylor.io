e83f22139fb468d211a6cedabf076f87f4f35ec9
Mike Taylor <mike.charles.taylor@gmail.com>
2023-12-21 18:12:34 -0700

    Change cache-control max-age

--- server/index.ts
+++ server/index.ts
@@ -61,7 +61,7 @@ async function startServer() {
     } else {
       const { body, statusCode, headers, earlyHints } = httpResponse
       if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
-      res.setHeader('Cache-Control', 'public, max-age=300')
+      res.setHeader('Cache-Control', 'public, max-age=14400')
       headers.forEach(([name, value]) => res.setHeader(name, value))
       res.status(statusCode)
       // For HTTP streams use httpResponse.pipe() instead, see https://vike.dev/stream
