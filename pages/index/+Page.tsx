42ea60f411c54a0bd499ef7af89b4fa4074b0d9b
Mike Taylor <mike.charles.taylor@gmail.com>
2024-02-05 22:29:37 -0700

    Add github link

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,10 +1,19 @@
 export { Page }
 
+import './Page.css'
+
 function Page() {
   return (
     <>
-      <h1>Mike Taylor</h1>
-      <p>Canadian Software Developer</p>
+      <h1 class="name">Mike Taylor</h1>
+      <p class="title">Canadian Software Developer</p>
+      <div class="social">
+        <a href="https://github.com/mtaylor91">
+          <i class="fa-brands fa-github fa-2x">
+            GitHub
+          </i>
+        </a>
+      </div>
     </>
   )
 }
--- renderer/+onRenderHtml.tsx
+++ renderer/+onRenderHtml.tsx
@@ -31,6 +31,7 @@ const onRenderHtml: OnRenderHtmlAsync = async (pageContext): ReturnType<OnRender
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/0363bbe277.js" crossorigin="anonymous"></script>
         <title>${title}</title>
       </head>
       <body>
