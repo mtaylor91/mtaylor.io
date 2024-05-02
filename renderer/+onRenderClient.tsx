8fcf6fba5df7e514689ff94683893320647617e1
Mike Taylor <mike.charles.taylor@gmail.com>
2024-05-02 15:17:45 -0600

    Disable legacy events socket connection

--- renderer/+onRenderClient.tsx
+++ renderer/+onRenderClient.tsx
@@ -5,8 +5,6 @@ import { hydrateRoot } from 'react-dom/client'
 import { PageShell } from './PageShell'
 import type { OnRenderClientAsync } from 'vike/types'
 
-import { getOrCreateClient } from '../socket'
-
 // This onRenderClient() hook only supports SSR, see https://vike.dev/render-modes for how to modify onRenderClient()
 // to support SPA
 const onRenderClient: OnRenderClientAsync = async (pageContext): ReturnType<OnRenderClientAsync> => {
@@ -14,8 +12,6 @@ const onRenderClient: OnRenderClientAsync = async (pageContext): ReturnType<OnRe
   if (!Page) throw new Error('Client-side render() hook expects pageContext.Page to be defined')
   const root = document.getElementById('preact-root')
   if (!root) throw new Error('DOM element #preact-root not found')
-  const client = await getOrCreateClient()
-  client.send('ce01f0a4-27ff-4bfe-bd5a-4140567fba9f', 'onRenderClient', new Uint8Array())
   hydrateRoot(
     root,
     <PageShell pageContext={pageContext}>
